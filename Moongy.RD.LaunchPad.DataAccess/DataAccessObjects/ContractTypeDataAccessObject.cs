b33b4c1521a2151c075a7dc5db4e1dcfa7eb767d
Fábio Jesus <fabioj@agap2.pt>
2025-02-04 13:42:17 +0000

    Added data access and interface

---
+++ Moongy.RD.LaunchPad.DataAccess/DataAccessObjects/ContractTypeDataAccessObject.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moongy.RD.Launchpad.Data.Contexts;
+using Moongy.RD.Launchpad.Data.Entities;
+using Moongy.RD.LaunchPad.DataAccess.Interfaces;
+
+namespace Moongy.RD.LaunchPad.DataAccess.DataAccessObjects
+{
+    public class ContractTypeDataAccessObject(LaunchpadContext context) : IContractTypeDataAccessObject
+    {
+        
+        private readonly LaunchpadContext _context = context;
+        public async Task<Guid> CreateAsync(ContractType contractType)
+        {
+            var result = await _context.AddAsync(contractType);
+            await _context.SaveChangesAsync();
+            return result.Entity.UUid;
+        }
+
+        public async Task DeleteAsync(ContractType contractType)
+        {
+            _context.Remove(contractType);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<ContractType?> GetAsync(Guid contractId)
+        {
+            var result = await _context.ContractTypes.Where(x => x.UUid == contractId).SingleOrDefaultAsync();
+            return result;
+        }
+
+        public async Task<IEnumerable<ContractType>> ListAsync()
+        {
+            var result = await _context.ContractTypes.ToListAsync();
+            return result;
+        }
+
+        public async Task<(int, IEnumerable<ContractType>)> ListAsync(int offset, int limit)
+        {
+            var result = await _context.ContractTypes.Skip(offset).Take(limit).ToListAsync();
+            var total = await _context.ContractTypes.CountAsync();
+            return (total, result);
+        }
+
+        public async Task UpdateAsync(ContractType contractType)
+        {
+            _context.Update(contractType);
+            await _context.SaveChangesAsync();
+        }
+
+
+    }
+}
---
+++ Moongy.RD.LaunchPad.DataAccess/Interfaces/IContractTypeDataAccessObject.cs
@@ -0,0 +1,13 @@
+﻿using Moongy.RD.Launchpad.Data.Entities;
+namespace Moongy.RD.LaunchPad.DataAccess.Interfaces;
+
+public interface IContractTypeDataAccessObject
+{
+    Task<Guid> CreateAsync(ContractType contractType);
+    Task<ContractType?> GetAsync(Guid contractId);
+    Task<IEnumerable<ContractType>> ListAsync();
+    Task UpdateAsync (ContractType contractType);
+    Task DeleteAsync (ContractType contractType);
+
+    Task<(int, IEnumerable<ContractType>)> ListAsync(int offset, int limit);
+}
