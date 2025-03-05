39cfcaa499284c134578536d9b68a4b146f0854c
DiogoFerreira2004 <up202205295@up.pt>
2025-03-05 16:46:04 +0000

    Non Fungible Composer

---
+++ Moongy.RD.Launchpad.ContractGenerator.ModelComposers.NonFungibleToken/Models/NonFungibleTokenModel.cs
@@ -0,0 +1,7 @@
+﻿using Moongy.RD.Launchpad.ContractGenerator.ModelComposers.NonFungibleToken.Models;
+using Moongy.RD.Launchpad.ModelComposers.ModelComposers.Core.Models;
+namespace Moongy.RD.Launchpad.ContractGenerator.ModelComposers.NonFungibleToken.Models;
+public class NonFungibleTokenModel : TokenModel
+{
+    
+}
