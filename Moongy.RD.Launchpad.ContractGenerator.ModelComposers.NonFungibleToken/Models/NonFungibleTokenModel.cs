1ec69adf0cde6338f11ab751525a6ed2b9dc42ee
DiogoFerreira2004 <up202205295@up.pt>
2025-03-05 17:30:51 +0000

    Fungible & NonFungible Models

--- Moongy.RD.Launchpad.ContractGenerator.ModelComposers.NonFungibleToken/Models/NonFungibleTokenModel.cs
+++ Moongy.RD.Launchpad.ContractGenerator.ModelComposers.NonFungibleToken/Models/NonFungibleTokenModel.cs
@@ -1,6 +1,18 @@
 ﻿using Moongy.RD.Launchpad.ModelComposers.ModelComposers.Core.Models;
+using System.Text.Json.Nodes;
 namespace Moongy.RD.Launchpad.ContractGenerator.ModelComposers.NonFungibleToken.Models;
 public class NonFungibleTokenModel : TokenModel
 {
-    
+    public string Name { get; set; }
+
+    public bool IsAccessible { get; set; }
+    public bool IsBurnable { get; set; }
+    public bool IsMintable { get; set; }
+    public bool IsPausable { get; set; }
+    public bool HasPermission { get; set; }
+    public bool IsUpgradable { get; set; }
+    public string URI { get; set; }
+    public string URIStorage { get; set; }
+    public bool Voting { get; set; }
+
 }
