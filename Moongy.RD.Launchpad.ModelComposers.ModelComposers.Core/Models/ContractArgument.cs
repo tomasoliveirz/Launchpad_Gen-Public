acdc0024226f9cd55aa76afd0f41f07dfb7f3787
fabiojesus <fabio@FABIO-DESK>
2025-03-07 18:31:03 +0000

    Added metamodel and template example (delete later)

---
+++ Moongy.RD.Launchpad.ModelComposers.ModelComposers.Core/Models/ContractArgument.cs
@@ -0,0 +1,12 @@
+﻿using Moongy.RD.Launchpad.ModelComposers.ModelComposers.Core.Enums;
+
+namespace Moongy.RD.Launchpad.ModelComposers.ModelComposers.Core.Models
+{
+    public class ContractArgument
+    {
+        public ContractDataType Type { get; set; }
+        public string? Name { get; set; }
+        public string? Value { get; set; }
+        public int Index { get; set; }
+    }
+}
