267a76ea45ecddd20227c62ac3b4e530716ef378
Bernardo Pombinho <bernardopombinho@gmail.com>
2025-03-17 09:46:35 +0000

    added icon in datalist

--- app/src/components/reUIsables/DataList/data-list.tsx
+++ app/src/components/reUIsables/DataList/data-list.tsx
@@ -2,10 +2,13 @@ import { Box, Flex } from "@chakra-ui/react"
 import { Link } from "react-router-dom";
 import { Text } from "@chakra-ui/react";
 import { JSX } from "react";
+import { IconType } from "react-icons";
+import { ImageOrIcon } from "../ImageOrIcon/image-or-icon";
 
 export interface DataListItemProps<T> {
     dataKey: keyof (T)
     label?: string
+    icon?: IconType | string
     dataType?: "text" | "number" | "amount",
     url?: string
     formatCell?: (s: T[keyof T]) => JSX.Element
@@ -20,16 +23,19 @@ export interface DataListProps<T> {
 export function DataList<T>({ columns, item }: DataListProps<T>) {
     return (
         <Flex w="100%" p="1em" flexDir="column" gap="1em">
-            {columns?.map(({ dataKey, label, url, formatCell, format }) => {
+            {columns?.map(({ dataKey, label, icon, url, formatCell, format }) => {
                 const content = formatCell
                     ? formatCell(item[dataKey])
                     : format
-                    ? format(String(item[dataKey]))
-                    : String(item[dataKey]);
+                        ? format(String(item[dataKey]))
+                        : String(item[dataKey]);
 
                 return (
                     <Box key={String(dataKey)}>
-                        <Text mr="0.5em" fontWeight="bold">{label}</Text>
+                        <Flex>
+                            {icon && <ImageOrIcon w="1.5em" mr="1em" value={icon} />}
+                            <Text mr="0.5em" fontWeight="bold">{label}</Text>
+                        </Flex>
                         {url ? (
                             <Link style={{ color: "white" }} to={url}>
                                 <Text fontWeight="thin">{content !== "" ? content : "None"}</Text>
