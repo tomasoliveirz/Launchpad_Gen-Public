a91620162c06c9f83dcc9175cb015598d19b18da
DiogoFerreira2004 <up202205295@up.pt>
2025-03-21 11:04:02 +0000

    Deflation and Tax Models

--- ContractGenerator/Tokenomics/Moongy.RD.Launchpad.ContractGenerator.Tokenomics.Deflation/Models/DeflationTokenomicModel.cs
+++ ContractGenerator/Tokenomics/Moongy.RD.Launchpad.ContractGenerator.Tokenomics.Deflation/Models/DeflationTokenomicModel.cs
@@ -3,4 +3,6 @@
 namespace Moongy.RD.Launchpad.ContractGenerator.Tokenomics.Deflation.Models;
 public class DeflationTokenomicModel : ITokenomic
 {
+    public decimal BurnPercentage { get; set; }
+    public ulong BurnAmount { get; set; }
 }
