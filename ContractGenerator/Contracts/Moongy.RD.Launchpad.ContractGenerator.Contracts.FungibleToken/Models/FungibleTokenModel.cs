54aedc7f0551daffa014f915853c148849034b31
PedroMMarinho <pmiguel.marinho@gmail.com>
2025-03-21 10:36:03 +0000

    Added FungibleTokenModel

--- ContractGenerator/Contracts/Moongy.RD.Launchpad.ContractGenerator.Contracts.FungibleToken/Models/FungibleTokenModel.cs
+++ ContractGenerator/Contracts/Moongy.RD.Launchpad.ContractGenerator.Contracts.FungibleToken/Models/FungibleTokenModel.cs
@@ -3,4 +3,11 @@
 namespace Moongy.RD.Launchpad.ContractGenerator.Contracts.FungibleToken.Models;
 public class FungibleTokenModel : IToken
 {
+    String? Symbol { get; set; }
+    Byte Decimals { get; set; }
+    ulong Circulation { get; set; }
+    bool HasAutoSwap { get; set; }
+    bool HasFlashMint { get; set; }
+    ulong PremintAmmount { get; set; }
+    bool HasTokenRecovery { get; set; }
 }
