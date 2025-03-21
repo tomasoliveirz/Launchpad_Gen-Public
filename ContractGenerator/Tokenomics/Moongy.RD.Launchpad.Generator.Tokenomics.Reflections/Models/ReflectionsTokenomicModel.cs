56e3772fa3fb1f08844851ac23468a7d658ea3c0
DiogoFerreira2004 <up202205295@up.pt>
2025-03-21 17:42:14 +0000

    Reflection Model Update

--- ContractGenerator/Tokenomics/Moongy.RD.Launchpad.Generator.Tokenomics.Reflections/Models/ReflectionsTokenomicModel.cs
+++ ContractGenerator/Tokenomics/Moongy.RD.Launchpad.Generator.Tokenomics.Reflections/Models/ReflectionsTokenomicModel.cs
@@ -1,7 +1,9 @@
-﻿using Moongy.RD.Launchpad.Generator.Tokenomics.Core.Interfaces;
+﻿using Moongy.RD.Launchpad.Core.Models;
+using Moongy.RD.Launchpad.Generator.Tokenomics.Core.Interfaces;
 
 namespace Moongy.RD.Launchpad.Generator.Tokenomics.Reflections.Models;
 public class ReflectionsTokenomicModel : ITokenomic
 {
     public decimal ReflectionsPercentage { get; set; }
+    public Dictionary<Address, ulong> UserClaimableBalances { get; set; } = new Dictionary<Address, ulong>();
 }
