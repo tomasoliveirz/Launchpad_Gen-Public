ae705e7e195365267793f069a6bfe1466d080c34
PedroMMarinho <pmiguel.marinho@gmail.com>
2025-03-28 10:44:36 +0000

    Added some attributes to SmartContractModel

---
+++ Moongy.RD.Launchpad.Core/Models/Import.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Moongy.RD.Launchpad.Core.Models
+{
+    public class Import
+    {
+        String? Name { get; set; }
+        String? Path { get; set; }
+    }
+}
--- Moongy.RD.Launchpad.Core/Models/SmartContractModel.cs
+++ Moongy.RD.Launchpad.Core/Models/SmartContractModel.cs
@@ -1,4 +1,7 @@
 ﻿namespace Moongy.RD.Launchpad.Core.Models;
 public class SmartContractModel
 {
+    String? Licence { get; set; }
+    SoftwareVersion? Version { get; set; }
+    List<Import> Imports { get; set; } = [];
 }
