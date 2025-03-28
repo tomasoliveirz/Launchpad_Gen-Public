4c7314720914df8c83aadc146ff56271eb30289f
PedroMMarinho <pmiguel.marinho@gmail.com>
2025-03-28 16:27:50 +0000

    Added contract operation

--- Moongy.RD.Launchpad.Core/Models/Metamodel/SmartContractModel.cs
+++ Moongy.RD.Launchpad.Core/Models/Metamodel/SmartContractModel.cs
@@ -9,5 +9,6 @@ public class SmartContractModel
     List<Import> Imports { get; set; } = [];
     public string? Name { get; set; }
     public List<Execution> Executions { get; set; } = [];  
-    public List<Event> Events { get; set; } = [];  
+    public List<Event> Events { get; set; } = [];
+    public List<ContractOperation> ConstructorOperations { get; set; } = [];
 }
