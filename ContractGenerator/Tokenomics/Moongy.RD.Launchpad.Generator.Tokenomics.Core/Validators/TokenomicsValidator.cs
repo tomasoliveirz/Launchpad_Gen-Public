26e8255c521672c7e84bf7477c349037b3df6575
tomy <up202208415@up.pt>
2025-04-11 11:06:28 +0100

    TokenomicsValidator Tests

--- ContractGenerator/Tokenomics/Moongy.RD.Launchpad.Generator.Tokenomics.Core/Validators/TokenomicsValidator.cs
+++ ContractGenerator/Tokenomics/Moongy.RD.Launchpad.Generator.Tokenomics.Core/Validators/TokenomicsValidator.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
 using Moongy.RD.Launchpad.Generator.Tokenomics.Core.Interfaces;
 using Moongy.RD.Launchpad.Generator.Tokenomics.Core.Attributes;
 using Moongy.RD.Launchpad.Generator.Tokenomics.Core.Enums;
@@ -11,17 +14,17 @@ namespace Moongy.RD.Launchpad.Generator.Tokenomics.Core.Validators
         private const double MAX_TOTAL_TAX_PERCENTAGE = 20.0;
         private const double MAX_TOTAL_WEIGHT = 100.0;
         
-        
-        // dicionario dos pesos a serem usados no manual mode
-        private static readonly Dictionary<string, double> OptimizedWeights = new()
+        // map do nome do tokenomic para o namespace e assembly do validador
+        private static readonly Dictionary<string, (string Namespace, string Assembly)> ValidatorInfo = new()
         {
-            { "TaxTokenomicModel", 20 },
-            { "DeflationTokenomicModel", 20 },
-            { "LiquidityGenerationTokenomicModel", 20 },
-            { "BuybackTokenomicModel", 20 },
-            { "ReflectionsTokenomicModel", 40 },
-            { "AntiWhaleTokenomicModel", 15 }
+            { "TaxTokenomicModel", ("Moongy.RD.Launchpad.Generator.Tokenomics.Tax.Validators.TaxTokenomicValidator", "Moongy.RD.Launchpad.Generator.Tokenomics.Tax") },
+            { "DeflationTokenomicModel", ("Moongy.RD.Launchpad.Generator.Tokenomics.Deflation.Validators.DeflationTokenomicValidator", "Moongy.RD.Launchpad.Generator.Tokenomics.Deflation") },
+            { "LiquidityGenerationTokenomicModel", ("Moongy.RD.Launchpad.Generator.Tokenomics.LiquidityGeneration.Validators.LiquidityGenerationTokenomicValidator", "Moongy.RD.Launchpad.Generator.Tokenomics.LiquidityGeneration") },
+            { "BuybackTokenomicModel", ("Moongy.RD.Launchpad.Generator.Tokenomics.Buyback.Validators.BuybackTokenomicValidator", "Moongy.RD.Launchpad.Generator.Tokenomics.Buyback") },
+            { "ReflectionsTokenomicModel", ("Moongy.RD.Launchpad.Generator.Tokenomics.Reflections.Validators.ReflectionsTokenomicValidator", "Moongy.RD.Launchpad.Generator.Tokenomics.Reflections") },
+            { "AntiWhaleTokenomicModel", ("Moongy.RD.Launchpad.Generator.Tokenomics.AntiWhale.Validators.AntiWhaleTokenomicValidator", "Moongy.RD.Launchpad.Generator.Tokenomics.AntiWhale") }
         };
+        
         public static void ValidateWeight(List<ITokenomic> tokenomics)
         {
             double totalWeight = 0;
@@ -69,15 +72,12 @@ namespace Moongy.RD.Launchpad.Generator.Tokenomics.Core.Validators
             bool hasBuyback = tokenomics.Any(t => t.GetType().Name.Contains("Buyback"));
             bool hasLiquidity = tokenomics.Any(t => t.GetType().Name.Contains("Liquidity"));
             
+            // aqui temos que verificar o modo de trigger tambem, como verificar?? caso seja manual mode até poderá ser viável
             if (hasReflection && hasBuyback && hasLiquidity)
                 throw new InvalidTokenomicException("Using Reflections, Buyback, and Liquidity together exceeds recommended computational limits.");
             
             if (hasReflection && tokenomics.Count > 3 && tokenomics.Any(t => t.TriggerMode == TokenomicTriggerMode.Automatic))
                 throw new InvalidTokenomicException("Reflections in automatic mode with more than 3 tokenomics may exceed computational limits. Consider using manual mode for Reflections.");
-                    
-            int taxingTokenomics = tokenomics.Count(t => t.TaxPercentage > 0);
-            if (taxingTokenomics >= 3 && tokenomics.Sum(t => t.TaxPercentage) > 15)
-                throw new InvalidTokenomicException("Multiple tokenomics with taxation approaching 20% may impact user experience negatively. Consider reducing tax percentages or using manual mode.");
         }
         
         private static double CalcManualModeWeight(ITokenomic tokenomic)
@@ -85,7 +85,7 @@ namespace Moongy.RD.Launchpad.Generator.Tokenomics.Core.Validators
             string tokenomicTypeName = tokenomic.GetType().Name;
     
             var attr = tokenomic.GetType().GetCustomAttribute<TokenomicAttribute>();
-            double baseWeight = attr.Weight;
+            double baseWeight = attr?.Weight ?? 0;
     
             if (tokenomicTypeName == "TaxTokenomicModel")
             {
@@ -110,24 +110,6 @@ namespace Moongy.RD.Launchpad.Generator.Tokenomics.Core.Validators
             return baseWeight;
         }
         
-        private static bool HasAssociatedTransfer(ITokenomic tokenomic, List<ITokenomic> allTokenomics)
-        {
-            string tokenomicName = tokenomic.GetType().Name;
-            
-            if (tokenomicName.Contains("Reflection"))
-            {
-                return allTokenomics.Any(t => t.GetType().Name.Contains("Tax") || 
-                                            t.GetType().Name.Contains("Liquidity"));
-            }
-            
-            if (tokenomicName.Contains("Buyback"))
-            {
-                return allTokenomics.Any(t => t.GetType().Name.Contains("Liquidity"));
-            }
-            
-            return false;
-        }
-
         public static void ValidateIndividualTokenomic(ITokenomic tokenomic)
         {
             if (tokenomic == null)
@@ -141,53 +123,70 @@ namespace Moongy.RD.Launchpad.Generator.Tokenomics.Core.Validators
             
             try
             {
-                switch (typeName)
+                if (!ValidatorInfo.TryGetValue(typeName, out var validatorData))
                 {
-                    case "TaxTokenomicModel":
-                        dynamic taxModel = tokenomic;
-                        dynamic taxValidator = Type.GetType("Moongy.RD.Launchpad.Generator.Tokenomics.Tax.Validators.TaxTokenomicValidator, Moongy.RD.Launchpad.Generator.Tokenomics.Tax");
-                        taxValidator.Validate(taxModel);
-                        break;
-                        
-                    case "DeflationTokenomicModel":
-                        dynamic deflationModel = tokenomic;
-                        dynamic deflationValidator = Type.GetType("Moongy.RD.Launchpad.Generator.Tokenomics.Deflation.Validators.DeflationTokenomicValidator, Moongy.RD.Launchpad.Generator.Tokenomics.Deflation");
-                        deflationValidator.Validate(deflationModel);
-                        break;
-                        
-                    case "LiquidityGenerationTokenomicModel":
-                        dynamic liquidityModel = tokenomic;
-                        dynamic liquidityValidator = Type.GetType("Moongy.RD.Launchpad.Generator.Tokenomics.LiquidityGeneration.Validators.LiquidityGenerationTokenomicValidator, Moongy.RD.Launchpad.Generator.Tokenomics.LiquidityGeneration");
-                        liquidityValidator.Validate(liquidityModel);
-                        break;
-                        
-                    case "BuybackTokenomicModel":
-                        dynamic buybackModel = tokenomic;
-                        dynamic buybackValidator = Type.GetType("Moongy.RD.Launchpad.Generator.Tokenomics.Buyback.Validators.BuybackTokenomicValidator, Moongy.RD.Launchpad.Generator.Tokenomics.Buyback");
-                        buybackValidator.Validate(buybackModel);
-                        break;
-                        
-                    case "ReflectionsTokenomicModel":
-                        dynamic reflectionsModel = tokenomic;
-                        dynamic reflectionsValidator = Type.GetType("Moongy.RD.Launchpad.Generator.Tokenomics.Reflections.Validators.ReflectionsTokenomicValidator, Moongy.RD.Launchpad.Generator.Tokenomics.Reflections");
-                        reflectionsValidator.Validate(reflectionsModel);
-                        break;
-                        
-                    case "AntiWhaleTokenomicModel":
-                        dynamic antiWhaleModel = tokenomic;
-                        dynamic antiWhaleValidator = Type.GetType("Moongy.RD.Launchpad.Generator.Tokenomics.AntiWhale.Validators.AntiWhaleTokenomicValidator, Moongy.RD.Launchpad.Generator.Tokenomics.AntiWhale");
-                        antiWhaleValidator.Validate(antiWhaleModel);
-                        break;
-                        
-                    default:
-                        break;
+                    throw new InvalidTokenomicException(
+                        $"No validator configuration found for tokenomic type: {typeName}");
                 }
+                
+                Type validatorType = null;
+                string fullTypePath = $"{validatorData.Namespace}, {validatorData.Assembly}";
+                validatorType = Type.GetType(fullTypePath);
+                
+                if (validatorType == null)
+                {
+                    foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+                    {
+                        if (assembly.GetName().Name == validatorData.Assembly)
+                        {
+                            validatorType = assembly.GetType(validatorData.Namespace);
+                            break;
+                        }
+                    }
+                }
+                
+                if (validatorType == null)
+                {
+                    string shortTypeName = validatorData.Namespace.Split('.').Last();
+                    foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+                    {
+                        foreach (var type in assembly.GetTypes())
+                        {
+                            if (type.Name == shortTypeName)
+                            {
+                                validatorType = type;
+                                break;
+                            }
+                        }
+                        if (validatorType != null)
+                            break;
+                    }
+                }
+                
+                if (validatorType == null)
+                {
+                    throw new InvalidTokenomicException(
+                        $"Could not find validator type for {typeName}. Validator: {validatorData.Namespace}");
+                }
+                
+                MethodInfo validateMethod = validatorType.GetMethod("Validate");
+                if (validateMethod == null)
+                {
+                    throw new InvalidTokenomicException(
+                        $"Validator for {typeName} does not have a Validate method.");
+                }
+                
+                validateMethod.Invoke(null, new object[] { tokenomic });
+            }
+            catch (TargetInvocationException ex) when (ex.InnerException is InvalidTokenomicException)
+            {
+                throw ex.InnerException;
             }
             catch (Exception ex) when (!(ex is InvalidTokenomicException))
             {
-                throw new InvalidTokenomicException($"Failed to validate {typeName}: {ex.Message}. Ensure appropriate validator is available.", ex);
+                throw new InvalidTokenomicException(
+                    $"Failed to validate {typeName}: {ex.Message}. Ensure appropriate validator is available.", ex);
             }
-            
         }
     }
 }
