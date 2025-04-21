7fdd8eae2f75ac78fdbad55ba685af64d7df052f
PedroMMarinho <pmiguel.marinho@gmail.com>
2025-04-21 16:49:35 +0100

    Started GenericMetamodelDecorator

--- Moongy.RD.Launchpad.Core/Attributes/MetaModelPropertyAttribute.cs
+++ Moongy.RD.Launchpad.Core/Attributes/MetaModelPropertyAttribute.cs
@@ -5,7 +5,7 @@ namespace Moongy.RD.Launchpad.Core.Attributes
     [AttributeUsage(AttributeTargets.Property)]
     public class MetaModelPropertyAttribute : Attribute
     {
-        public string Name { get; set; } = "";
+        public string? Name { get; set; }
         public PropertyType PropertyType { get; set; }
         public DataType DataType { get; set; }
         public VisibilityModifier Visibility { get; set; } = VisibilityModifier.None;
