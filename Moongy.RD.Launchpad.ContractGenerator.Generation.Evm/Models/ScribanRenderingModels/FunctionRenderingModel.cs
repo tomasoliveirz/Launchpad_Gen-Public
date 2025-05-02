b3dc2820f1bd8de26c6bb7f9b91263d3bb9cdffc
PedroMMarinho <pmiguel.marinho@gmail.com>
2025-05-02 18:37:46 +0100

    Added function scriban template;Needs fine tuning; Resolved some smalls issues; Next will correct errors

---
+++ Moongy.RD.Launchpad.ContractGenerator.Generation.Evm/Models/ScribanRenderingModels/FunctionRenderingModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Moongy.RD.Launchpad.ContractGenerator.Generation.Evm.Models.ScribanRenderingModels
+{
+    public class FunctionRenderingModel
+    {
+        public required string Name { get; set; }
+        public string[] Parameters { get; set; } = [];
+        public required string Body { get; set; }
+        public string Visibility { get; set; } = "";
+        public string Mutability { get; set; } = "";
+    }
+}
