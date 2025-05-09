318670a02a0fb9685871b07cfeb8dd6930fe36a5
fabiojesus <fabio@FABIO-DESK>
2025-05-09 12:48:04 +0100

    Added Prebuilt solidity components

---
+++ Moongy.RD.Launchpad.ContractGenerator.Generation.Evm/Prebuilds/PermitPrebuild.cs
@@ -0,0 +1,20 @@
+﻿using Moongy.RD.Launchpad.ContractGenerator.Generation.Evm.Models.Metamodels.Errors;
+using Moongy.RD.Launchpad.ContractGenerator.Generation.Evm.Models.Metamodels.Parameters;
+
+namespace Moongy.RD.Launchpad.ContractGenerator.Generation.Evm.Prebuilds
+{
+    public static class PermitPrebuild
+    {
+
+        private static List<ErrorParameterModel> _eRC2612ExpiredSignatureParameters = [new() { Name = "deadline", Type = DataTypePreBuild.Uint256 }];
+        public static ErrorModel ERC2612ExpiredSignature => new ErrorModel() { Name = "ERC2612ExpiredSignature", Parameters = _eRC2612ExpiredSignatureParameters };
+
+        private static List<ErrorParameterModel> _eRC2612InvalidSignerParameters = [
+            new() { Name = "signer", Type = DataTypePreBuild.Address },
+            new() { Name = "owner", Type = DataTypePreBuild.Address }];
+
+        public static ErrorModel ERC2612InvalidSigner => new ErrorModel() { Name = "ERC2612ExpiredSignature", Parameters = _eRC2612InvalidSignerParameters };
+
+
+    }
+}
