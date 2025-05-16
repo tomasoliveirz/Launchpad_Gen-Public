3192a7d38e7092818ae8047ed1b624b69d2bedcb
tomy <up202208415@up.pt>
2025-05-16 12:15:42 +0100

    Added ContextProperties to the NFT Standard

--- Moongy.RD.Launchpad.CodeGenerator.Standards/Models/NonFungibleTokenModel.cs
+++ Moongy.RD.Launchpad.CodeGenerator.Standards/Models/NonFungibleTokenModel.cs
@@ -1,16 +1,24 @@
-﻿using Moongy.RD.Launchpad.CodeGenerator.Standards.Enums;
+﻿using Moongy.RD.Launchpad.CodeGenerator.Core.Attributes;
+using Moongy.RD.Launchpad.CodeGenerator.Core.Metamodels.Others;
+using Moongy.RD.Launchpad.CodeGenerator.Standards.Enums;
 
 namespace Moongy.RD.Launchpad.CodeGenerator.Standards.Models;
 
 public class NonFungibleTokenModel : BaseContractModel
 {
+    [ContextProperty(Name = "isEnumerable", Type = PrimitiveType.Bool, Visibility = Visibility.Public, HasDefaultValue = true)]
     public bool IsEnumerable { get; set; }
-
+    
+    [ContextProperty(Name = "hasURI", Type = PrimitiveType.Bool, Visibility = Visibility.Public, HasDefaultValue = true)]
     public bool HasURI { get; set; }
 
+    [ContextProperty(Name = "uri", Type = PrimitiveType.String, Visibility = Visibility.Public, HasDefaultValue = true)]
     public string? URI { get; set; }
-
+    
+    // como fazer com o type aqui?
+    [ContextProperty(Name = "uriStorageType", Visibility = Visibility.Public, HasDefaultValue = true)]
     public UriStorageType URIStorageType { get; set; }
 
+    [ContextProperty(Name = "uriStorageLocation", Type = PrimitiveType.String, Visibility = Visibility.Public, HasDefaultValue = true)]
     public string? URIStorageLocation { get; set; }
 }
