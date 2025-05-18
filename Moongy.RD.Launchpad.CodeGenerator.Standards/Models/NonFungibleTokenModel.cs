2994adfcd375590dad52ee6ab556483649f96999
tomy <up202208415@up.pt>
2025-05-18 14:14:51 +0100

    Cleaned wrong context properties

--- Moongy.RD.Launchpad.CodeGenerator.Standards/Models/NonFungibleTokenModel.cs
+++ Moongy.RD.Launchpad.CodeGenerator.Standards/Models/NonFungibleTokenModel.cs
@@ -7,19 +7,13 @@ namespace Moongy.RD.Launchpad.CodeGenerator.Standards.Models;
 
 public class NonFungibleTokenModel : BaseContractModel, IUriStorable
 {
-    [ContextProperty(Name = "isEnumerable", Type = PrimitiveType.Bool, Visibility = Visibility.Public, HasDefaultValue = true)]
     public bool IsEnumerable { get; set; }
     
-    [ContextProperty(Name = "hasURI", Type = PrimitiveType.Bool, Visibility = Visibility.Public, HasDefaultValue = true)]
     public bool HasURI { get; set; }
 
-    [ContextProperty(Name = "uri", Type = PrimitiveType.String, Visibility = Visibility.Public, HasDefaultValue = true)]
     public string? URI { get; set; }
     
-    // como fazer com o type aqui?
-    [ContextProperty(Name = "uriStorageType", Visibility = Visibility.Public, HasDefaultValue = true)]
     public UriStorageType URIStorageType { get; set; }
 
-    [ContextProperty(Name = "uriStorageLocation", Type = PrimitiveType.String, Visibility = Visibility.Public, HasDefaultValue = true)]
     public string? URIStorageLocation { get; set; }
 }
