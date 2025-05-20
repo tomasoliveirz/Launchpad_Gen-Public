2d7f52e5f1679606e3ea056ac1d3425e69d97b5a
tomy <up202208415@up.pt>
2025-05-20 09:15:01 +0100

    TaxTokenomicValidator updated

--- Moongy.RD.Launchpad.CodeGenerator.Tokenomics/Validators/Tax/TaxTokenomicValidator.cs
+++ Moongy.RD.Launchpad.CodeGenerator.Tokenomics/Validators/Tax/TaxTokenomicValidator.cs
@@ -1,21 +1,30 @@
 ﻿using Moongy.RD.Launchpad.CodeGenerator.Tokenomics.Models.Tax;
 using System.ComponentModel.DataAnnotations;
+using Moongy.RD.Launchpad.CodeGenerator.Core.Interfaces;
 
 namespace Moongy.RD.Launchpad.CodeGenerator.Tokenomics.Validators.Tax
 {
-    public static class TaxTokenomicValidator
+    public class TaxTokenomicValidator : IValidator<TaxTokenomicModel>
     {
-        public static void Validate(TaxTokenomicModel model)
+        private const double MaxAllowedTax = 5;
+        public void Validate(TaxTokenomicModel model)
         {
             if (model == null)
                 throw new ArgumentNullException(nameof(model));
 
-            if (model.TaxFee < 0 || model.TaxFee > 100)
-                throw new ValidationException("Tax: TaxFee must be between 0 and 100.");
+            if (model.TaxFee < 0 || model.TaxFee > MaxAllowedTax)
+                throw new ValidationException($"Tax: TaxFee must be between 0 and {MaxAllowedTax}.");
 
             if (model.TaxRecipients == null || !model.TaxRecipients.Any())
                 throw new ValidationException("Tax: You must define at least one TaxRecipient.");
 
+
+            var duplicate = model.TaxRecipients
+                                                            .GroupBy(x => x.Address)
+                                                            .FirstOrDefault(g => g.Count() > 1);
+            if (duplicate != null)
+                throw new ValidationException($"Tax: Duplicate TaxRecipient found for address {duplicate.Key}.");
+            
             decimal totalShares = 0;
             foreach (var recipient in model.TaxRecipients)
             {
