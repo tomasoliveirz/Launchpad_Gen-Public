773bd7c2467764967e5adcb0ebe8b8ce2646032d
tomy <up202208415@up.pt>
2025-05-20 14:27:17 +0100

    AccessControlExtensionModel updated

--- Moongy.RD.Launchpad.CodeGenerator.Extensions/Models/AccessControlExtensionModel.cs
+++ Moongy.RD.Launchpad.CodeGenerator.Extensions/Models/AccessControlExtensionModel.cs
@@ -7,8 +7,6 @@ public class AccessControlExtensionModel
 {
     [ContextProperty(Name = "owner", Type = PrimitiveType.Address, Visibility = Visibility.Public)]
     public string? Owner { get; set; }
-    
-    [ContextProperty(Name = "hasRoles", Type = PrimitiveType.Bool, Visibility = Visibility.Public)]
     public bool HasRoles { get; set; }
     
     // this does not go directly to the context -- the augmenter will probably convert it to a mapping
