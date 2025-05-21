a6a9681b3401fafb810c267f0b3718633df5d808
tomy <up202208415@up.pt>
2025-05-21 21:26:04 +0100

    AccessControlExtension Validator done

--- Moongy.RD.Launchpad.CodeGenerator.Extensions/Validators/AccessControlExtensionValidator.cs
+++ Moongy.RD.Launchpad.CodeGenerator.Extensions/Validators/AccessControlExtensionValidator.cs
@@ -6,10 +6,25 @@ namespace Moongy.RD.Launchpad.CodeGenerator.Extensions.Validators
 {
     public class AccessControlExtensionValidator : IValidator<AccessControlExtensionModel>
     {
+        private const string ZeroAddress = "0x0000000000000000000000000000000000000000";
+
         public void Validate(AccessControlExtensionModel obj)
         {
+            
             if (obj == null)
-                throw new ArgumentNullException(nameof(obj));
+                throw new ArgumentNullException(nameof(obj), "AccessControlExtensionModel cannot be null.");
+            
+            if(string.IsNullOrWhiteSpace(obj.Owner))
+                throw new ValidationException("Owner cannot be null or empty.");
+            
+            if (obj.Owner.Equals(ZeroAddress, StringComparison.OrdinalIgnoreCase))
+                throw new ValidationException("Owner cannot be the zero address.");
+
+            if (obj.Roles.Count > 0)
+            {
+                if (obj.Roles.Distinct(StringComparer.OrdinalIgnoreCase).Count() != obj.Roles.Count)
+                    throw new ValidationException("Roles must be unique.");
+            }
         }
 
     }
