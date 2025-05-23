f2da73b5ad84762edacb1085f07e505155f132b2
tomy <up202208415@up.pt>
2025-05-23 14:51:57 +0100

    Merge fixed

---
+++ Moongy.RD.Launchpad.CodeGenerator.Core/Metamodels/Helpers/ContextHelpers.cs
@@ -0,0 +1,6 @@
+namespace Moongy.RD.Launchpad.CodeGenerator.Core.Metamodels.Helpers;
+
+public class ContextHelpers
+{
+    
+}
--- Moongy.RD.Launchpad.CodeGenerator.Standards/Composers/Base/DataTypeReference.cs
+++ Moongy.RD.Launchpad.CodeGenerator.Standards/Composers/Base/DataTypeReference.cs
@@ -7,10 +7,7 @@ namespace Moongy.RD.Launchpad.CodeGenerator.Standards.Composers.Base
         public static TypeReference Address => new TypeReference() { Kind = TypeReferenceKind.Simple, Primitive = PrimitiveType.Address };
         public static TypeReference Uint256 => new TypeReference() { Kind = TypeReferenceKind.Simple, Primitive = PrimitiveType.Uint256 };
         public static TypeReference String => new TypeReference() { Kind = TypeReferenceKind.Simple, Primitive = PrimitiveType.String };
-<<<<<<< HEAD
         public static TypeReference Bool => new TypeReference() { Kind = TypeReferenceKind.Simple, Primitive = PrimitiveType.Bool };
-=======
         public static TypeReference Uint8 => new TypeReference() { Kind = TypeReferenceKind.Simple, Primitive = PrimitiveType.Uint8 };
->>>>>>> b0ce180fb95450112f555c2e58734f2084b080cf
     }
 }
