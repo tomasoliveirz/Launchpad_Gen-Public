08b757aa8c9998b63490cd551c069fc6e5a46526
PedroMMarinho <pmiguel.marinho@gmail.com>
2025-05-30 09:17:44 +0100

    Copied Exceptions from main

---
+++ Moongy.RD.Launchpad.CodeGenerator.Generation.Evm/Exceptions/InvalidTemplateException.cs
@@ -0,0 +1,5 @@
+﻿namespace Moongy.RD.Launchpad.ContractGenerator.Generation.Evm.Exceptions;
+
+public class InvalidTemplateException(string path) : Exception($"No template found at {path}. Make sure to check it as Embedded on the Build Action")
+{
+}
